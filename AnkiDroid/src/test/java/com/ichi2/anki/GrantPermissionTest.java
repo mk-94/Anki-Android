096006300356d3d067926c04f29effc2131f8fb9
Mario <mario.kirsch94@web.de>
2021-04-19 00:36:40 +0200

    Implementation DeckPickerTest grant permission at initial startup but AnkiActivity mock does not work.

--- AnkiDroid/src/test/java/com/ichi2/anki/GrantPermissionTest.java
+++ AnkiDroid/src/test/java/com/ichi2/anki/GrantPermissionTest.java
@@ -1,5 +1,7 @@
 package com.ichi2.anki;
 
+import android.Manifest;
+import android.app.Application;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
@@ -8,12 +10,18 @@ import com.ichi2.anki.dialogs.DatabaseErrorDialog;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Shadows;
+import org.robolectric.shadows.ShadowApplication;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.not;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import timber.log.Timber;
 
@@ -23,10 +31,20 @@ public class GrantPermissionTest extends RobolectricTest {
 
     @Test
     public void deckPickerShouldHavePermissionAfterFirstGrant() {
+
+        //Start the App for the first time
+        //Push OK-Button for the welcome message
+        //Give initial permissions (Press allow)
+        //Check if DIALOG_LOAD_FAILED will not be shown
+
+        AnkiActivity mock = mock(AnkiActivity.class);
+        when(mock.colIsOpen()).thenReturn(false);
         GrantPermissionTest.DeckPickerEx d = super.startActivityNormallyOpenCollectionWithIntent(GrantPermissionTest.DeckPickerEx.class, new Intent());
-        InitialActivityTest.setupForDatabaseConflict();
+        Application application = ApplicationProvider.getApplicationContext();
+        ShadowApplication app = Shadows.shadowOf(application);
+        app.grantPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE);;
         d.onStoragePermissionGranted();
-        assertThat("Permission granted Database Error Log should not be shown", d.mDatabaseErrorDialog, not(DatabaseErrorDialog.DIALOG_LOAD_FAILED));
+        assertThat("Permission granted and no dialog should be shown", d.mDatabaseErrorDialog, nullValue());
 
     }
 
